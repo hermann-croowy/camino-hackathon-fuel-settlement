5b877120b7c2459730052998f85f78c31375171a
Luis Jimenez <lu.jim@protonmail.com>
2025-11-26 18:38:17 +0100

    feat: integrate UI into project

--- front-end/vite-project/src/App.jsx
+++ front-end/vite-project/src/App.jsx
@@ -1,16 +1,39 @@
-import { Navbar, Welcome, Footer, Services, Transactions} from  './components';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { Navbar, Welcome, Footer, Services, Transactions, CreateOrderForm, OrdersList } from './components';
+
+const AppContent = () => {
+  const location = useLocation();
+  const isCreateOrderPage = location.pathname === '/create-order';
+  const isOrdersPage = location.pathname === '/orders';
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className={`gradient-bg-welcome ${isCreateOrderPage || isOrdersPage ? 'flex-1 flex flex-col min-h-screen' : ''}`}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/create-order" element={<CreateOrderForm />} />
+          <Route path="/orders" element={<OrdersList />} />
+        </Routes>
+      </div>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <Services />
+            <Transactions />
+          </>
+        } />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
 
 const App = () => {
   return (
-    <div className= "min-h-screen">
-      <div className="gradient-bg-welcome">
-        <Navbar />
-        <Welcome />
-      </div>
-      <Services />
-      <Transactions />
-      <Footer />
-    </div>
+    <Router>
+      <AppContent />
+    </Router>
   );
 }
 
